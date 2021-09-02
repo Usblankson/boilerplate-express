3238a097aef497e4f7246016299cbd49a940419a
Usblankson <uyohoiniblankson11@gmail.com>
2021-09-02 02:37:15 +0100

    added solution to Task 4

--- myApp.js
+++ myApp.js
@@ -1,11 +1,25 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res) {
-  //res.send("Hello Express");
-  res.sendFile(__dirname + "/views/index.html");
-});
-
+ // Task 1
 //console.log("Hello World");
+
+  // Task  2
+// app.get('/', function(req, res){
+//   res.send("Running Script");
+// });
+
+  // Task 3
+// app.get("/", function(req, res) {
+//   res.sendFile(__dirname + "/views/index.html");
+// });
+
+//  Task 4
+// Normal usage
+//app.use(express.static(__dirname + "/public"));
+
+//Assets at the /public route Usage
+app.use("/public", express.static(__dirname + "/public"));
+
 
 module.exports = app;
